e6449141fbb67431f43fe0f3211fe285fd1dd430
anas soodokeev <agentanas1@gmail.com>
2023-06-17 16:13:13 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/lazy_oad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lazy_oad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:lazy_oad/ui/widgets/app/my_app.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  const app = MyApp();
+  runApp(app);
+}
